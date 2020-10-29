277abe8578b0304d8220e14afa289e302efc72a4
Camilo Andres Chavez <camilo.andreschavez@globaltranz.com>
2020-10-29 13:51:13 -0500

    Initial commit MasGlobal Technical Test

--- MasGlobal.Employees/masglobal.employees.web/src/Employee/EmployeeList.js
+++ MasGlobal.Employees/masglobal.employees.web/src/Employee/EmployeeList.js
@@ -9,7 +9,7 @@ export default class EmployeeList extends Component {
     }
     componentDidMount() {
         debugger;
-        var apiUrl = 'https://localhost:44301/Employees'
+        var apiUrl = 'https://localhost:5001/employees'
         this.setState({ employeeId: this.props.employeeId });
         if (this.props.employeeId) {
             apiUrl = apiUrl + "/" + this.props.employeeId
